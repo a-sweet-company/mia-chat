f6fe1bce794c31d3dd346512c87d70421428f636
Rauan Bernardo Ribeiro de Araujo <rauanbernardo@gmail.com>
2024-12-11 16:08:46 -0300

    feat - Created error 404 view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="not-found">
+      <h1>404 - Página Não Encontrada</h1>
+      <p>Ops! Parece que a página que você está procurando não existe.</p>
+      <router-link to="/">Voltar para a Página Inicial</router-link>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NotFoundView',
+  };
+  </script>
+  
+  <style scoped>
+  .not-found {
+    text-align: center;
+    margin-top: 50px;
+  }
+  
+  h1 {
+    font-size: 3em;
+    color: #ff4d4f;
+  }
+  
+  p {
+    font-size: 1.2em;
+    margin: 20px 0;
+  }
+  
+  a {
+    color: #3498db;
+    text-decoration: none;
+    font-weight: bold;
+  }
+  
+  a:hover {
+    text-decoration: underline;
+  }
+  </style>
+  
