80d897496dcf46f08eb08c0c01fa77bcf15a4444
Rauan Bernardo Ribeiro de Araujo <rauanbernardo@gmail.com>
2024-12-14 16:25:50 -0300

    Update - Header
    Fixed header hover, added jost font to global css, fixed header formatting.

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -6,8 +6,8 @@
         :key="option.name"
         :to="option.path"
         :class="{ active: option.name === activeOption, hover: hoveredOption === option.name }"
-        @mouseover="hoveredOption = option.name"
-        @mouseleave="hoveredOption = null"
+        @mouseover.native="hoveredOption = option.name"
+        @mouseleave.native="hoveredOption = null"
         @click="setActive(option.name)"
       >
         {{ option.name }}
@@ -24,16 +24,17 @@ export default {
       options: [
         { name: 'Home', path: '/' },
         { name: 'Chat', path: '/chat' },
-        { name: 'Settings', path: '/settings' },
+        { name: 'Perfil', path: '/perfil' },
+        {name: 'Sobre', path: '/sobre'}
       ],
-      hoveredOption: null,
+      hoveredOption: null
     };
   },
   methods: {
     setActive(option) {
       this.$emit('update:activeOption', option);
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -42,10 +43,10 @@ export default {
 .header {
   display: flex;
   justify-content: center; /* Centraliza horizontalmente o conteúdo */
-  background-color: #f4f4f4;
+  background-color: var(--branco);
   padding: 15px 20px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  width: 100%;
+  width: 100vw;
+  font-family: "Jost", sans-serif; /* Aplica a fonte Jost ao header */
 }
 
 /* Estilo da navegação */
@@ -59,13 +60,18 @@ export default {
   color: #333;
   text-decoration: none;
   padding: 10px 15px;
-  border-radius: 5px;
+  border-radius: 10px;
   transition: all 0.3s ease; /* Suaviza a transição */
+  margin-right: 5vw; 
+  border: solid 1.5px #2E2E2E;
+  padding-right: 10vw;
+  font-family: "Jost", sans-serif; /* Aplica a fonte Jost aos links */
+  
 }
 
 /* Estilo para o link ativo */
 .header nav a.active {
-  color: #fff;
+  color: #ffffff;
   background-color: #1199CE;
   font-weight: bold;
 }
@@ -73,7 +79,9 @@ export default {
 /* Efeito de hover nos links */
 .header nav a.hover {
   color: #fff;
-  background-color: #1199CE; /* Cor de fundo no hover */
+  background-color: #2E2E2E; /* Cor de fundo no hover */
+  transform: scale(1.1); /* Aumenta o tamanho do link ao passar o mouse */
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2); /* Adiciona uma sombra ao hover */
 }
 
 /* Estilo responsivo para telas menores */
