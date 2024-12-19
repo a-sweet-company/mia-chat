d1b5b424b5c2f8237e8f5f2d15d7f9ad22c04835
hpsouza1 <hpsouza1@stefanini.com>
2024-12-19 11:30:59 -0300

    corrected modal

--- src/components/IntroModal.vue
+++ src/components/IntroModal.vue
@@ -1,86 +1,114 @@
 <template>
-    <transition name="fade">
-      <div v-if="showIntroModal" class="intro-modal">
-        <span class="close" @click="closeIntroModal">&times;</span>
-        <div class="modal-header">
-          <img src="../assets/teste.png" alt="Generic Photo" class="modal-image">
-          <h2>Olá,</h2>
-        </div>
-        <div class="modal-body">
-          <p>Sou a <strong>Mia</strong>, sua assistente de apoio emocional e psicológico. Estou pronta te ouvir e te ajudar, vamos começar?</p>
-        </div>
-        <div class="modal-footer">
-          <button class="btn-primary" @click="closeIntroModal">Claro!</button>
+  <transition name="fade">
+    <div v-if="showIntroModal" class="intro-modal">
+      <span class="close" @click="closeIntroModal">&times;</span>
+      <div class="modal-content">
+        <img src="../assets/Mia-Modal.png" alt="Mia-Modal" class="modal-image">
+        <div class="modal-text">
+          <h1 class="modal-greeting"><strong>Olá,</strong></h1>
+          <p class="modal-description">
+            Sou a <strong>Mia</strong>, sua assistente de apoio emocional e psicológico.
+            Estou pronta para te ouvir e te ajudar. Vamos começar?
+          </p>
         </div>
       </div>
-    </transition>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      showIntroModal: {
-        type: Boolean,
-        required: true
-      }
-    },
-    methods: {
-      closeIntroModal() {
-        this.$emit('close-intro-modal');
-      }
+      <div class="modal-footer">
+        <button class="btn-primary" @click="closeIntroModal">Claro!</button>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  props: {
+    showIntroModal: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    closeIntroModal() {
+      this.$emit("close-intro-modal");
     }
   }
-  </script>
-  
-  <style scoped>
-  .intro-modal {
-    position: fixed;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    background-color: var(--color-branco);
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);
-    z-index: 1000;
-    border-color: var(--color-preto);
-  }
-  
-  .modal-header {
-    display: flex;
-    align-items: center;
-  }
-  
-  .modal-image {
-    width: 150px;
-    height: 150px;
-    margin-right: 10px;
-  }
-  
-  .modal-body {
-    margin-top: 10px;
-  }
-  
-  .modal-footer {
-    margin-top: 20px;
-    text-align: right;
-  }
-  
-  .btn-primary {
-    background-color: var(--color-azul);
-    color: var(--color-branco);
-    border: none;
-    padding: 10px 20px;
-    border-radius: 5px;
-    cursor: pointer;
-  }
-  
-  .close {
-    position: absolute;
-    top: 10px;
-    right: 10px;
-    font-size: 20px;
-    cursor: pointer;
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+.intro-modal {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: none;
+  padding: 10px;
+  border-radius: 8px;
+  box-shadow: 0 4px 20px rgba(0, 0, 0, 0.15);
+  z-index: 1000;
+  width: 500px;
+  height: 258px;
+  border: 2px solid var(--color-preto);
+  display: flex;
+  flex-direction: column;
+}
+
+.modal-content {
+  display: flex;
+  align-items: center;
+  height: 70%;
+}
+
+.modal-image {
+  width: 190px;
+  height: auto;
+  margin-right: 10px;
+  margin-top: 110px;
+}
+
+.modal-text {
+  text-align: left;
+  flex: 1;
+}
+
+.modal-greeting {
+  font-size: 34px;
+  margin: 0;
+}
+
+.modal-description {
+  font-size: 16px;
+  margin: 10px 0 0 0;
+}
+
+.modal-footer {
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  height: 20%;
+}
+
+.btn-primary {
+  background-color: var(--color-azul);
+  color: var(--color-branco);
+  border: none;
+  padding: 10px 50px;
+  border-radius: 10px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s ease;
+}
+
+.btn-primary:hover {
+  background-color: var(--color-azul-escuro);
+}
+
+.close {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  font-size: 18px;
+  cursor: pointer;
+  color: var(--color-preto);
+}
+</style>
